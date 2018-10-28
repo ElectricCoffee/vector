07d7a856d2692dd4ed46ddb46357882556eb81d6
Niko Lepka <slench102+git@gmail.com>
2018-10-28 20:53:17 +0100

    Add module level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! # vector
+//! 
+//! Vector is a crate for providing simple and easy to use vectors for use in game development in the style of those found in the Unity game engine.
+//! 
+//! The library provides three things out of the box: the `Vector` trait, which contains methods like `normalize`, `lerp`, and `zero`, which are common across all vector implementations; the `Vector2` struct, which provides a base-implementation for a 2D vector with `f64` fields; and finally `Vector3`, which provides a base-implementation of a 3D vector, also with `f64` fields.
+
 use std::ops;
 
 pub mod vector2;
@@ -6,6 +12,7 @@ pub use vector2::Vector2;
 pub mod vector3;
 pub use vector3::Vector3;
 
+/// Base Vector trait.
 pub trait Vector: 
     ops::Add 
     + ops::AddAssign 
